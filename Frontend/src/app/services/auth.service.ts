5b872a9ce26ab379678855ab6287f77481d22fdf
Miguel Mena Correa <mmenacor@everis.com>
2019-11-06 20:03:51 -0500

    instalacion de firebase y angular/fire

--- Frontend/src/app/services/auth.service.ts
+++ Frontend/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { UserModel } from '../models/user.model';
+import { FirebaseFirestore } from '@angular/fire';
 
 
 @Injectable({
@@ -10,7 +11,9 @@ export class AuthService {
   private url = 'https://identitytoolkit.googleapis.com/v1';
   private apiKey = 'AIzaSyC88QqO3nhYFlpiAphFzNmPCZWFwKLHH2Q';
 
-  constructor( private http: HttpClient) { }
+  constructor( private http: HttpClient,
+               private database: FirebaseFirestore
+    ) { }
 
   logout() {
 
@@ -21,9 +24,10 @@ export class AuthService {
       ...usuario,
       returnSecureToken: true
     };
+    const db = this.database.collection('/inspeccion_ascensor');
+    console.log('Resultado', db);
 
     return this.http.post(`${ this.url }/accounts:signInWithPassword?key=${ this.apiKey }`, authData);
-
   }
 
   nuevoUsuario(usuario: UserModel) {
