6bfd51144a2c05920428836d49b80ab896140205
Miguel Mena Correa <mmenacor@everis.com>
2019-11-12 21:31:50 -0500

    modelo inspeccion ascensores

--- Frontend/src/app/views/inspection-menu/inspection-menu.page.ts
+++ Frontend/src/app/views/inspection-menu/inspection-menu.page.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { collectionMock } from './mock';
 @Component({
   selector: 'app-inspection-menu',
   templateUrl: './inspection-menu.page.html',
   styleUrls: ['./inspection-menu.page.scss'],
 })
 export class InspectionMenuPage implements OnInit {
-
+  public inspeccion = collectionMock;
+  public cabecera = this.inspeccion.cabecera;
+  public cabina = this.inspeccion.listas_de_verificacion;
   constructor() { }
 
   ngOnInit() {
+    console.log(this.inspeccion);
   }
 
 }
