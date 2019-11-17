973f794bec79beea9412f9ee40e348b035570cb6
juankmiloh <juankmiloh@hotmail.com>
2019-11-17 14:26:15 -0500

    Se crean rutas de las tabs de ascensor

--- Frontend/src/app/views/inspection-list/tabs-routing.module.ts
+++ Frontend/src/app/views/inspection-list/tabs-routing.module.ts
@@ -4,10 +4,14 @@ import { InspectionListPage } from './inspection-list.page';
 
 const routes: Routes = [
   { 
-    path: 'tabs/:equipo',
+    path: 'tabs/ascensor',
     component: InspectionListPage,
     children:[
-      { path: 'cabina', loadChildren: '../inspection-list/tabs/cabina/cabina.module#CabinaPageModule' }
+      {
+        path: '',
+        redirectTo: '/inspection-list/tab/cabina',
+        pathMatch: 'full'
+      }
     ],
   },
   {
