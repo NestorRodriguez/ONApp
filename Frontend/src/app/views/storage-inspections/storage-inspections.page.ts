386e207043dd494ae7d5741932571edda4bfc457
Miguel Mena Correa <mmenacor@everis.com>
2019-12-11 23:08:52 -0500

    carga de imagenes al storage y replace url en la coleccion a guardar

--- Frontend/src/app/views/storage-inspections/storage-inspections.page.ts
+++ Frontend/src/app/views/storage-inspections/storage-inspections.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { QueriesService } from 'src/app/services/queries.service';
 import { LoadingController, ToastController } from '@ionic/angular';
+import { AngularFireStorageModule, AngularFireStorage } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-storage-inspections',
@@ -15,11 +16,13 @@ export class StorageInspectionsPage implements OnInit {
   success: boolean;
   isData: boolean;
   mensajeError: string;
+  urls: any[] = [];
   mensajeSuccess: string;
   constructor( private storage: Storage,
                private serviceFirebase: QueriesService,
                public loadingController: LoadingController,
-               public toastController: ToastController ) { }
+               public toastController: ToastController,
+               public firebaseStorage: AngularFireStorage ) { }
 
   ngOnInit() {
     console.log('entre a la vista');
@@ -45,25 +48,8 @@ export class StorageInspectionsPage implements OnInit {
     });
 
     await loading.present();
-    for ( const inspeccion of this.inspecciones) {
-    await this.serviceFirebase.createInspection(inspeccion).then( () => {
-      this.success = true;
-    }, (error) => {
-      this.success = false;
-      console.log('Ocurrió un error al guardar la inspección', error);
-    });
-  }
-      // console.log(this.jsonInspection);
+    await this.uploadPictures();
     await loading.dismiss();
-    if (this.success) {
-      this.inspecciones = [];
-      this.storage.set('inspecciones_ascensores', this.inspecciones);
-      const mensajeSuccess = 'Inspecciones guardadas con éxito';
-      this.presentToast(mensajeSuccess);
-    } else {
-      const mensajeError = 'Ocurrió un error, inténtalo más tarde';
-      this.presentToast(mensajeError);
-    }
 
   }
 
@@ -94,7 +80,6 @@ export class StorageInspectionsPage implements OnInit {
     if (this.success) {
       this.presentToast(this.mensajeSuccess);
     } else {
-      
       this.presentToast(this.mensajeError);
     }
 
@@ -106,6 +91,53 @@ export class StorageInspectionsPage implements OnInit {
         duration: 2000
       });
       toast.present();
+    }
+
+    async uploadPictures() {
+      for ( const inspeccion of this.inspecciones) {
+        const inspeccionId = inspeccion.datos_basicos.c_consecutivo;
+        console.log(inspeccion);
+        const listas = inspeccion.lista_verificacion;
+        const keys = Object.keys(listas);
+        const categorias: any[] = Object.values(listas);
+        for( const categoria of categorias ) {
+          const ruta = keys[categorias.indexOf(categoria)];
+          for ( const item of categoria) {
+            const index = (categoria.indexOf(item) + 1);
+            if ( item.fotografias.length > 0 ) {
+              for ( const foto of item.fotografias) {
+              const indexFoto = (item.fotografias.indexOf(foto) + 1);
+              const indexUrl = item.fotografias.indexOf(foto);
+              const guardarImagenes = this.firebaseStorage.ref(`pictures/inspeccion#${inspeccionId}/${ruta}/item${index}-${indexFoto}`);
+              const url = await guardarImagenes.putString(foto, 'data_url').then( async () => {
+                const link = guardarImagenes.getDownloadURL().subscribe( async (urlfoto) => {
+                  item.fotografias[indexUrl] = urlfoto;
+                  this.urls.push(urlfoto);
+                });
+              });
+          }
+        } else {
+          console.log('No hay fotografías para cargar');
+        }
+      }
+        }
+        await this.serviceFirebase.createInspection(inspeccion).then( () => {
+          this.success = true;
+        }, (error) => {
+          this.success = false;
+          const msgerror = 'Ocurrió un error al guardar la inspección' + error;
+          this.presentToast(msgerror);
+        });
+        if (this.success) {
+          this.inspecciones = [];
+          this.storage.set('inspecciones_ascensores', this.inspecciones);
+          const mensajeSuccess = `Inspeccion ${inspeccionId} guardadas con éxito`;
+          this.presentToast(mensajeSuccess);
+        } else {
+          const mensajeError = 'Ocurrió un error, inténtalo más tarde ';
+          this.presentToast(mensajeError);
+    }
+      }
     }
 
   }
